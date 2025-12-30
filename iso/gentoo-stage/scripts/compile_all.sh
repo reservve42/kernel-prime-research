6635c1ac732845d089521a7c6928b9d2eaaff440
heanrig <henriqueeeee85@gmail.com>
2025-12-30 09:28:00 -0300

    Refactor compile_all.sh for improved structure
    rewrite script into "compile_all.sh"

--- iso/gentoo-stage/scripts/compile_all.sh
+++ iso/gentoo-stage/scripts/compile_all.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
-# compile all research binaries
+set -e
 
-echo "[*] compiling C++ binaries"
-g++ -O2 -Wall -Wextra /opt/research/cpp/main.cpp /opt/research/cpp/core/bigint.cpp /opt/research/cpp/core/prime.cpp -o /opt/research/bin/prime_cpp
-g++ -O2 -Wall -Wextra /opt/research/experiments/timing/prime_timing.cpp -o /opt/research/bin/prime_timing
-g++ -O2 -Wall -Wextra /opt/research/experiments/factorization/pollard_rho.cpp -o /opt/research/bin/pollard_rho
+BIN=bin
+mkdir -p $BIN
 
-echo "[*] compiling Fortran binaries"
-gfortran -O2 /opt/research/fortran/main.f90 /opt/research/fortran/modules/bigint_mod.f90 /opt/research/fortran/modules/prime_mod.f90 -o /opt/research/bin/prime_fortran
+echo "[*] compiling C++ core"
 
-echo "[*] compilation done"
+g++ -std=c++20 -O2 -Wall -Wextra \
+    -Icpp/include \
+    cpp/core/main.cpp \
+    cpp/core/bigint.cpp \
+    cpp/core/prime.cpp \
+    cpp/tools/benchmarks.cpp \
+    -o $BIN/prime_cpp
 
+echo "[*] compiling C++ experiments"
+
+g++ -std=c++20 -O2 -Wall -Wextra \
+    -Icpp/include \
+    experiments/timing/prime_timing.cpp \
+    cpp/core/bigint.cpp \
+    cpp/core/prime.cpp \
+    -o $BIN/prime_timing
+
+g++ -std=c++20 -O2 -Wall -Wextra \
+    -Icpp/include \
+    experiments/factorization/pollard_rho.cpp \
+    cpp/core/bigint.cpp \
+    cpp/core/prime.cpp \
+    -o $BIN/pollard_rho
+
+echo "[*] compiling Fortran"
+
+gfortran -O2 \
+    fortran/modules/bigint_mod.f90 \
+    fortran/modules/prime_mod.f90 \
+    fortran/main.f90 \
+    -o $BIN/prime_fortran
+
+echo "[+] build finished successfully"
