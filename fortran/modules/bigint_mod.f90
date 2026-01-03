379bd181a67491dee9d9e468259f78d57f379a86
heanrig <henriqueeeee85@gmail.com>
2026-01-03 16:27:11 -0300

    Update bigint_mod.f90 with project details
    Added project documentation and copyright notice.

--- fortran/modules/bigint_mod.f90
+++ fortran/modules/bigint_mod.f90
@@ -1,3 +1,22 @@
+! Kernel Cryptography Research Project
+!
+! Low-level cryptography algorithms and protocols
+! for Linux kernel experimentation.
+! 
+! Project focuses on:
+!      - Prime number computations
+!      - Cryptographic number theory
+!      - Computational hardness
+!      - Encrypted data handling and protocol tracing (e.g., CP2A)
+! 
+!  Documentation and research notes are maintained internally.
+! 
+!  Copyright (c) 2025 Luís Henrique (Heanrig) <henriqueeeee85@gmail.com>
+! 
+!  This project is intended for research and educational purposes.
+!  Redistribution and modification are allowed under GPL-2.0-or-later.
+
+
 module bigint_mod
     implicit none
     integer, parameter :: dp = selected_int_kind(18)
@@ -8,4 +27,3 @@ contains
         res = a * b
     end function mul
 end module bigint_mod
-
